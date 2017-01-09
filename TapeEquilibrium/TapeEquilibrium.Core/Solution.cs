d76a90984e4087c40bd51dcedb664bf3b260d02c
slanto <slawek.antos@outlook.com>
2017-01-09 12:10:49 +0100

    Fake implementation added

---
+++ TapeEquilibrium/TapeEquilibrium.Core/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace TapeEquilibrium.Core
+{
+    public class Solution
+    {
+        public int GetTapeEquilibrium(int[] a)
+        {
+            return 1;
+        }
+    }
+}
---
+++ TapeEquilibrium/TapeEquilibrium.Tests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace TapeEquilibrium.Tests
+{
+    using Core;
+
+    using Xunit;
+
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 1, 2, 4, 3 }, 1)]
+        public void GetTapeEquilibrium_ReturnsMinimalDifferenceForGivenArray(int[] array, int expected)
+        {
+            var sut = new Solution();
+            var actual = sut.GetTapeEquilibrium(array);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
