cfb128f481accbb0c22a9d9628d3ae97ef8d7c6c
slanto <slawek.antos@outlook.com>
2017-01-09 12:33:43 +0100

    Naive implementation. https://codility.com/demo/results/training3RH3FR-MDS/

--- TapeEquilibrium/TapeEquilibrium.Core/Solution.cs
+++ TapeEquilibrium/TapeEquilibrium.Core/Solution.cs
@@ -1,10 +1,29 @@
 ﻿namespace TapeEquilibrium.Core
 {
+    using System;
+
     public class Solution
     {
         public int GetTapeEquilibrium(int[] a)
         {
-            return 1;
+            int minimalDiff = -1;
+            var right = 0;
+            var left = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                left += a[i];
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    right += a[j];
+                }
+                var diff = Math.Abs(left - right);
+                right = 0;
+                if (diff < minimalDiff || minimalDiff == -1)
+                {
+                    minimalDiff = diff;
+                }
+            }
+            return minimalDiff;
         }
     }
 }
--- TapeEquilibrium/TapeEquilibrium.Tests/SolutionTests.cs
+++ TapeEquilibrium/TapeEquilibrium.Tests/SolutionTests.cs
@@ -8,6 +8,8 @@
     {
         [Theory]
         [InlineData(new int[] { 3, 1, 2, 4, 3 }, 1)]
+        [InlineData(new int[] { 3, 1, 2, 1, 3, 5 }, 1)]
+        [InlineData(new int[] { 5, 1, 3 }, 1)]
         public void GetTapeEquilibrium_ReturnsMinimalDifferenceForGivenArray(int[] array, int expected)
         {
             var sut = new Solution();
